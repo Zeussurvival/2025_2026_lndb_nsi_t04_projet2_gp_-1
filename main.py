4a3fa7e7dc474186ba3d13db6547f2505257207e
Zeussurvival <emiljokui@gmail.com>
2026-02-18 08:27:49 +0100

    updt pr futur batiments

--- main.py
+++ main.py
@@ -159,9 +159,10 @@ pollution_max_possible = pollution_initiale
 Nom_image_list_tiles = ["background_1.png","background_2.png","Bush_tile.png","pollution_texture.png","transparent.png"]
 List_tiles = [CT.Tile(Nom_image_list_tiles[0],None,0),CT.Tile(Nom_image_list_tiles[0],None,90),CT.Tile(Nom_image_list_tiles[0],None,180),CT.Tile(Nom_image_list_tiles[0],None,270),\
               CT.Tile(Nom_image_list_tiles[1],None,0),CT.Tile(Nom_image_list_tiles[1],None,90),CT.Tile(Nom_image_list_tiles[1],None,180),CT.Tile(Nom_image_list_tiles[1],None,270),\
-              CT.Tile(Nom_image_list_tiles[2],None,0),
-              CT.Tile(Nom_image_list_tiles[3],None,0),
-              CT.Tile(Nom_image_list_tiles[4],None,0)]
+              CT.Tile(Nom_image_list_tiles[2],None,0),]
+
+liste_tile_needed = [CT.Tile(Nom_image_list_tiles[3],None,0),CT.Tile(Nom_image_list_tiles[4],None,0)]
+List_tiles += liste_tile_needed
 
 for y in range(Actual_map.shape[0]):
     for x in range(Actual_map.shape[1]):
