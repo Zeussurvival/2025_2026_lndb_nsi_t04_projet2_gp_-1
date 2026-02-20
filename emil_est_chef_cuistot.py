4bbf31126cd555a1a085d7d0dc82a87bace0b287
Zeussurvival <emiljokui@gmail.com>
2026-02-20 15:21:03 +0100

    Image to tile finis
    juste a changer le nom des images enregistrés et c good

--- emil_est_chef_cuistot.py
+++ emil_est_chef_cuistot.py
@@ -1,27 +1,52 @@
 from PIL import Image
-import pygame
+import os
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+temp_dir = os.path.join(main_dir,"Temp")
+test_map_dir = os.path.join(main_dir,"Test map json")
+print(main_dir)
 
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-image = Image.open("Test map json/bat3.png")
+
+image = Image.open("Test map json/bat3.png").convert("RGBA")
 print("Taille de l'image:", image.size)
 print("Mode de couleur:", image.mode)
 print("Format de l'image:", image.format)
 
-for i in range (5):
-    zone = (0+8*i, 0, 8+8*i, 8)
+Liste_des_tiles_de_limage = []
+
+def donne_le_nombre_si_inf_ou_egale_sinon_modulo(nombre,modu_a):
+    if nombre % modu_a == 0:
+        return modu_a
+    return nombre%modu_a
+
+
+for i in range ((image.size[0]//8)*(image.size[1]//8)):
+    changed = False
+    zone = (8*i%image.size[0], 8*(8*i//image.size[0]), donne_le_nombre_si_inf_ou_egale_sinon_modulo(8*(i+1),(image.size[0])), 8+8*((8*i)//image.size[0]))
+    print(zone)
     image_coupee = image.crop(zone)
-    text = "Temp/"+str(i)+"_image.png"
-    image_coupee.save((text))
+    list_image = os.listdir(temp_dir)
+    for A, filename in enumerate(os.listdir(temp_dir)):
+        path = os.path.join(temp_dir, filename)
+        img_exist = Image.open(path).convert("RGBA")
+
+        if image_coupee.size == img_exist.size and \
+        image_coupee.tobytes() == img_exist.tobytes():
+            Liste_des_tiles_de_limage.append(f"Temp/{filename}")
+            changed = True
+            break
+    print(changed,i)
+    if not changed:
+        text = "Temp/"+str(i)+"_image.png"
+        Liste_des_tiles_de_limage.append("Temp/"+str(i)+"_image.png")
+        image_coupee.save((text))
 
 
+print(Liste_des_tiles_de_limage)
+print((9*10)%19,(9*10)/19,(9*10)//19)
+print(os.path.join(main_dir,"spritesheet.png"))
 
-
-
-
-
-
-
+print(Image.open("Temp/4_image.png").convert("RGBA").tobytes()) # (96, 64, 104, 72)
+print(image.crop((96, 64, 104, 72)).tobytes())
+# img = Image.open(os.path.join(temp_dir,"36_image.png")).convert("RGBA")
+# img.show()
